593328c1fde3ae00d56066beff9d08d064d866da
takeshitakagi <takagitakeshi2@gmail.com>
2025-06-05 08:18:41 +0900

    line-clampに修正

--- src/PostListItem.jsx
+++ src/PostListItem.jsx
@@ -8,22 +8,17 @@ function PostListItem({ post }) {
   const day = date.getDay();
   const formattedDate = `${year}/${month}/${day}`;
 
-  // サムネイルに表示される本文を制御
-  const maxLength = 60;
-  const truncatedContent = post.content.substring(0, maxLength);
-  const finalContent = post.content.length > maxLength ? truncatedContent + '...' : post.content;
-
   return (
 
-    <div class='w-3/4 m-auto p-9 border-1 border-gray-700'>
+    <div class='w-3/4 m-auto p-9 border border-gray-700'>
       <div class='flex justify-between mx-auto container mb-3'>
       <p class='text-gray-500 text-sm'>{formattedDate}</p>
         <div class='text-gray-700'>{post.categories.map((category) => (
-          <span key={category} class="m-3 text-blue-600 border-1 border-blue-600 rounded-xs p-1.5">{category}</span>
+          <span key={category} class="m-2 text-blue-600 border border-blue-600 p-1.5 rounded-md">{category}</span>
       ))}</div>
       </div>
       <h2 class='text-gray-700 text-2xl font-medium mb-3'>{post.title}</h2>
-      <p class='text-gray-700' dangerouslySetInnerHTML={{__html: finalContent}}/>
+      <p class='text-gray-700 line-clamp-2' dangerouslySetInnerHTML={{__html: post.content}}/>
 
     </div>
 
