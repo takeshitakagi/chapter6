b24ffbb3a6fd6524846a0ef14cd7fb2fb1834bfb
takeshitakagi <takagitakeshi2@gmail.com>
2025-06-09 13:36:13 +0900

    読み込み中表示処理追加

--- src/PostList.jsx
+++ src/PostList.jsx
@@ -5,26 +5,37 @@ import PostListItem from "./PostListItem";
 function PostList() {
 
   const [posts, setPosts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(false);
 
   useEffect(() => {
     const fetcher = async () => {
+      setIsLoading(false);
+      setError(false);
       try {
         const res = await fetch("https://1hmfpsvto6.execute-api.ap-northeast-1.amazonaws.com/dev/posts")
         if (!res.ok) {
           throw new Error(`HTTP error! status: ${res.status}`);
         }
-        const data = await res.json()
-        setPosts(data.posts)
+        const data = await res.json();
+        setPosts(data.posts);
       } catch (error) {
         console.error("一覧の取得に失敗しました:", error);
         setPosts([]);
         setError(true);
+      } finally {
+        setIsLoading(true);
       }
     }
 
     fetcher()
   }, [])
+
+  if (!isLoading) {
+    return (
+      <div>記事を読み込み中...</div>
+    );
+  }
 
   if (error) {
     return (
